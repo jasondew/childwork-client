bc39a32c0d6b3c5b4a88f1f694062021174abe04
Jason Dew <jason.dew@gmail.com>
2015-04-26 19:11:52 -0400

    Fixing up chore [un]completion

--- app/components/chore-week.js
+++ app/components/chore-week.js
@@ -4,33 +4,44 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: "tr",
   actions: {
-    complete: function(dateString) {
-      var chore = this.get("chore"),
-          store = chore.get("store"),
+    complete: function(choreDay) {
+      var store = choreDay.get("store"),
           completedChore = store.createRecord("completed-chore", {
-            chore_id: this.get("chore.id"),
-            completed_on: moment(dateString).toDate()
+            chore: choreDay.get("chore"),
+            completed_on: choreDay.get("date")
           });
 
       completedChore.save();
     },
-    uncomplete: function(dateString) {
-      //TODO: implement
+    uncomplete: function(choreDay) {
+      var store = choreDay.get("store");
+
+      store.find("completed-chore", {chore: choreDay.get("chore"), completed_on: choreDay.get("date")})
+           .then(function(completedChore) {
+             completedChore.destroyRecord();
+           });
     }
   },
 
+  week: function() {
+    var beginningOfWeek = moment(this.get("today")).startOf("week").subtract(1, "day");
+
+    return [0, 1, 2, 3, 4, 5, 6].map(function(offset) {
+      return moment(beginningOfWeek).add(offset, "days");
+    });
+  }.property("today"),
+
   choreDays: function() {
     var chore = this.get("chore"),
-        today = this.get("today"),
         store = this.get("chore").store,
-        completedDates = chore.get("completedChores").map(function(completedChore) {
+        completedDates = chore.get("completedChores").filter(function(completedChore) {
+          return completedChore.get("completed_on") !== null;
+        }).map(function(completedChore) {
           return completedChore.get("completed_on").format("YYYY-MM-DD");
         });
 
-    console.log(completedDates);
-    return [0, 1, 2, 3, 4, 5, 6].map(function(offset) {
-      var date = moment(today).add(offset, "days"),
-          formattedDate = date.format("YYYY-MM-DD"),
+    return this.get("week").map(function(date) {
+      var formattedDate = date.format("YYYY-MM-DD"),
           complete = completedDates.indexOf(formattedDate) !== -1;
 
       return store.createRecord("chore-day", {
@@ -39,5 +50,5 @@ export default Ember.Component.extend({
         complete: complete
       });
     });
-  }.property("chore", "today"),
+  }.property("today", "chore.completedChores.@each.completed_on"),
 });
