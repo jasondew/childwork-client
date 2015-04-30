65fdbfb736446985d249413d63c080dea4482cbe
Jason Dew <jason.dew@gmail.com>
2015-04-29 20:52:57 -0400

    Fixing weekly income

--- app/controllers/chart.js
+++ app/controllers/chart.js
@@ -27,4 +27,26 @@ export default Ember.Controller.extend({
   formattedToday: function() {
     return this.get("today").format("LL");
   }.property("today"),
+
+  thisWeek: function(date) {
+    if (date === undefined) { return false; }
+
+    var today = this.get("today"),
+        endOfLastWeek = moment(today).startOf("week").subtract(2, "day"),
+        endOfThisWeek = moment(today).endOf("week").subtract(1, "day");
+
+    return date.isBetween(endOfLastWeek, endOfThisWeek, "day");
+  },
+
+  totalIncome: function() {
+    return this.model.chores.reduce(function(sum, chore) {
+      var completedChores = chore.get("completedChores"),
+          rate = chore.get("rate"),
+          countThisWeek = completedChores.filter(function(completedChore) {
+            return this.thisWeek(completedChore.get("completed_on"));
+          }.bind(this)).length;
+
+      return sum + rate * countThisWeek;
+    }.bind(this), 0);
+  }.property("model.chores.@each.completedChores"),
 });
--- app/models/chore.js
+++ app/models/chore.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   completedChores: DS.hasMany("completed-chore"),
   name: DS.attr("string"),
-  rate: DS.attr("string"),
+  rate: DS.attr("number"),
   notes: DS.attr("string"),
 });
